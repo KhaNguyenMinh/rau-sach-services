15485d5a6ae8aa726472d91d63f6a71bd5309256
Kha Nguyen Minh <khanm@blockchainlabs.asia>
2018-11-06 15:04:06 +0700

    Init nodejs server

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,5 @@
+import User from "./User/";
+
+// const typeDefs = [User];
+
+export default User;
---
+++ index.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import schema from "./graphql/ index";
+
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+
+const app = express();
+const PORT = process.env.PORT || "4000";
+const db = "mongodb://admin:admin123@ds253713.mlab.com:53713/rau_sach";
+
+// Connect to MongoDB with Mongoose.
+mongoose
+  .connect(
+    db,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+  app.use(
+    "/graphql",
+    cors(),
+    bodyParser.json(),
+    expressGraphQL({
+      schema,
+      graphiql: true
+    })
+  );
+  
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
